28d05921e6d47c76d185769ade7dee497d43a1c0
ajmarrocco <ajmarrocco@gmail.com>
2022-03-03 18:59:21 -0500

    updated schema and seeds sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,15 @@
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(12,2),
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,9 @@ INSERT INTO department (name)
         ('Sales'),
         ('Service'),
         ('Administration');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Software Engineer', 100000, 1),
+    ('Account Manager', 75000, 3),
+    ('Sales Rep', 50000, 2);
